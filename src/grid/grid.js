e087dc13bfbb935404080679d5d8f70d2a990c8a
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-07 16:25:04 +0100

    creating images folder and grid file links images

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -8,8 +8,28 @@ class Grid extends React.Component {
       <div className="grid-container">
         <div className="images-container">
           <figure>
-            <img src="" alt="image"/>
-            <figcaption></figcaption>
+            <img src="/images/board.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/bulb.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/calculation.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/calendar.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/code.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/text.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/thumbsUp.jpeg" alt="" />
+          </figure>
+          <figure>
+            <img src="/images/time.jpeg" alt="" />
           </figure>
         </div>
       </div>
