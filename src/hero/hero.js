01dbaa4fdd1ae5ef3f0e50437e69af207063ae10
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-07 14:37:58 +0100

    header css, app css docs updated and creating hero files

---
+++ src/hero/hero.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./hero.css"
+
+class Hero extends React.Component {
+
+  render() {
+    return (
+      <div className="hero-container">
+        hej
+      </div>
+    )
+  }
+}
+
+export default Hero
