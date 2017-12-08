e46ddacae3e909ccefbb45465891a311284cc189
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-08 06:01:18 +0100

    hero docs file updated

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -6,15 +6,15 @@ class Hero extends React.Component {
   render() {
     return (
       <div className="hero-container">
-        <div className="first-container">
+        <div className="first-container share">
           <h1>
-            <span>J</span><span>i</span><span>m</span><span>m</span><span>a </span>
-            <span>B</span><span>e</span><span>l</span><span>l</span><span>e </span>
-            <span>B</span><span>l</span><span>i</span><span>k</span><span>s</span>
+            <span>J</span><span>i</span><span>m</span><span>m</span><span>a</span> <span>B</span>
+            <span>e</span><span>l</span><span>l</span><span>e</span> <span>B</span>
+            <span>l</span><span>i</span><span>k</span><span>s</span>
             <span>t</span><span>a</span><span>d</span>
           </h1>
         </div>
-        <div className="second-container">
+        <div className="second-container share">
           <h1>
             <span>W</span><span>e</span><span>b</span> <span>D</span><span>e</span>
             <span>v</span><span>e</span><span>l</span><span>o</span><span>p</span>
