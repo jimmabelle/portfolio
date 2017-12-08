add4f972fc99acda198cc8c03eb28f7ba2f63d12
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-08 13:51:06 +0100

    component name updated

--- src/header/header.js
+++ src/header/header.js
@@ -1,18 +1,23 @@
 import React from "react"
+import { BrowserRouter, Route, Link } from "react-router-dom"
 import "./header.css"
 
 class Header extends React.Component {
 
   render() {
     return (
-      <div className="header-container">
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">About me</a></li>
-          <li><a href="">Project</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-      </div>
+      <BrowserRouter>
+        <div className="header-container">
+          <ul>
+            <li><Link to="/header">Home</Link></li>
+            <li><Link to="/">About me</Link></li>
+            <li><Link to="/">Project</Link></li>
+            <li><Link to="/">Contact</Link></li>
+          </ul>
+
+          <Route path="/header" component={Header} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
