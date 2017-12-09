f80cdd1eeb15522e9388f77e94808cbf62be4fd5
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-09 12:34:24 +0100

    contacts updated

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -4,14 +4,16 @@ import "./index.css"
 class Contact extends React.Component {
   render() {
     return (
-      <div className="form-style-6">
-        <h1>Contact Us</h1>
-        <form>
-          <input type="text" name="field1" placeholder="Your Name" />
-          <input type="email" name="field2" placeholder="Email Address" />
-          <textarea name="field3" placeholder="Type your Message" />
-          <input type="submit" value="Send" />
-        </form>
+      <div className="form-container">
+        <div className="form">
+          <h2>Contact Us</h2>
+          <form>
+            <input type="text" name="field1" placeholder="Your Name" />
+            <input type="email" name="field2" placeholder="Email Address" />
+            <textarea name="field3" placeholder="Type your Message" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
       </div>
     )
   }
