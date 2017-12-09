6ef4e714837469e041cb86c2b33716dd144b73ca
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-09 21:44:36 +0100

    creating projects components

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from "react-router-dom"
 
 import Home from "./home/index"
 import About from "./about/index"
+import Projects from "./projects/index"
 import Contact from "./contact/index"
 
 class App extends React.Component {
@@ -15,11 +16,12 @@ class App extends React.Component {
           <ul className="homePage-list">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About me</Link></li>
-            <li><Link to="/project">Projects</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
           <Route path="/" exact component={Home} />
           <Route path="/About" component={About} />
+          <Route path="/Projects" component={Projects} />
           <Route path="/Contact" component={Contact} />
         </div>
       </BrowserRouter>
---
+++ src/components/projects/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./index.css"
+
+class Projects extends React.Component {
+
+  render() {
+    return (
+      <div className="projects-container">
+        <p>Hello</p>
+      </div>
+    )
+  }
+
+}
+
+export default Projects
