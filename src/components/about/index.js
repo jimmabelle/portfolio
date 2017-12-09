49ed32435d66112a6d97f0957d7be534d04f80ea
jimmabelle <jimmabelle@users.noreply.github.com>
2017-12-09 08:33:58 +0100

    creating list items  and  updating styles

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -4,21 +4,36 @@ import "./index.css"
 class About extends React.Component {
   render() {
     return (
-      <div className="about-container">
+      <div className="aboutme-container">
         <div className="left">
           <figure>
             <img src="/images/cv.jpg" alt="Jimma" />
             <figcaption>
-              <p>Web Developer</p>
+              Web Developer
             </figcaption>
           </figure>
         </div>
         <div className="right">
           <ul className="aboutme-list">
             <h3>Jimma Belle Blikstad</h3>
-            <li>Adress:</li>
-            <li>Working Experiences:</li>
-            <li>Educational Attended:</li>
+            <li>
+              Adress:<br />
+              <span>Ursviksvägen 16, 172 36 Sundbyberg</span>
+            </li>
+            <li>Working Experiences:<br />
+              <span>
+                <p>Sales Associates</p>
+                <p>Secretary</p>
+              </span>
+            </li>
+            <li>Educational Attended:<br />
+              <span>
+                <p>Technigo</p>
+                <p>Changemaker Education</p>
+                <p>Komxux</p>
+                <p>University of Mindanao</p>
+              </span>
+            </li>
           </ul>
         </div>
       </div>
