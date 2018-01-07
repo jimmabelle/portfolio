51d744eb0ecc23161b63c4387559621372ca1441
jimmabelle <jimmabelle@users.noreply.github.com>
2018-01-07 07:52:54 +0100

    creating project data and accessing the data to project project preview

--- src/assets/project-preview/index.js
+++ src/assets/project-preview/index.js
@@ -6,6 +6,7 @@ class ProjectPreview extends React.Component {
 
   render() {
     return (
+      // created Link from the projects data
       <Link to={`/projects/${this.props.url}`} className="ProjectPreview">
         <div className="inner">
           <h1>{this.props.name}</h1>
