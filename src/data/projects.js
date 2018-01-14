128b8584799f65e6f5189dbe7c9f0a1b93a21f04
jimmabelle <jimmabelle@users.noreply.github.com>
2018-01-14 13:11:20 +0100

    css style updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,42 +10,42 @@ export default [
     name: "Hero",
     description: "Create a responsive hero banner",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/book.jpeg"
   }, {
     id: "radio-player",
     name: "Radio Player",
     description: "A radio player using the Sveriges Radio API",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/building.jpeg"
   }, {
     id: "yearbook",
     name: "Yearbook",
     description: "Build a yearbook using floats",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/bulb.jpeg"
   }, {
-    id: "yearbook",
-    name: "Yearbook",
-    description: "Build a yearbook using floats",
+    id: "flexbox",
+    name: "Flexbox",
+    description: "Build a yearbook using flexbox",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/calculation.jpeg"
   }, {
-    id: "yearbook",
-    name: "Yearbook",
-    description: "Build a yearbook using floats",
+    id: "sign-up-form",
+    name: "Sign-up Form",
+    description: "Building a sign-up form",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/calendar.jpeg"
   }, {
-    id: "yearbook",
-    name: "Yearbook",
-    description: "Build a yearbook using floats",
+    id: "faq",
+    name: "FAQ",
+    description: "Build a faq and make an accordion",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/code.jpeg"
   }, {
-    id: "yearbook",
-    name: "Yearbook",
-    description: "Build a yearbook using floats",
+    id: "weather-dashborad",
+    name: "Weather Dashboard",
+    description: "Build a dashboard using Api",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/board.jpeg"
+    image: "images/keyboard.jpeg"
   }
 ]
