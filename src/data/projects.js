9f8a01e188d2fcccab7f0b159fdbcf41fa6c7c71
jimmabelle <jimmabelle@users.noreply.github.com>
2018-01-15 20:10:13 +0100

    exporting data updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,13 +3,13 @@ export default [
     id: "yearbook",
     name: "Yearbook",
     description: "Build a yearbook using floats",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    link: "https://jimmabelle.github.io/assignment-yearbook",
     image: "images/board.jpeg"
   }, {
     id: "hero",
     name: "Hero",
     description: "Create a responsive hero banner",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    link: "https://jimmabelle.github.io/assignment-hero/",
     image: "images/book.jpeg"
   }, {
     id: "radio-player",
@@ -18,34 +18,53 @@ export default [
     link: "https://jimmabelle.github.io/assignment-radio-player/",
     image: "images/building.jpeg"
   }, {
-    id: "yearbook",
-    name: "Yearbook",
-    description: "Build a yearbook using floats",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    id: "design-brief",
+    name: "Design Brief",
+    description: "Build a homepage as close as original",
+    link: "https://jimmabelle.github.io/assignment-design-brief//",
     image: "images/bulb.jpeg"
   }, {
     id: "flexbox",
     name: "Flexbox",
     description: "Build a yearbook using flexbox",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    link: "https://jimmabelle.github.io/assignment-flexbox/",
     image: "images/calculation.jpeg"
   }, {
-    id: "sign-up-form",
-    name: "Sign-up Form",
-    description: "Building a sign-up form",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    id: "form",
+    name: "Form",
+    description: "Building a form",
+    link: "https://jimmabelle.github.io/assignment-forms/",
     image: "images/calendar.jpeg"
   }, {
     id: "faq",
     name: "FAQ",
     description: "Build a faq and make an accordion",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    link: "https://jimmabelle.github.io/assignment-accordion/",
     image: "images/code.jpeg"
   }, {
     id: "weather-dashborad",
     name: "Weather Dashboard",
     description: "Build a dashboard using Api",
-    link: "https://jimmabelle.github.io/assignment-radio-player/",
+    link: "https://jimmabelle.github.io/assignment-weather/",
     image: "images/keyboard.jpeg"
+  }, {
+    id: "todo-app",
+    name: "To Do App",
+    description: "Build a dashboard using Api",
+    link: "https://jimmabelle.github.io/assignment-todo-app-2/",
+    image: "images/metal.jpeg"
+  }, {
+    id: "vat-calculator",
+    name: "Vat Calculator",
+    description: "Build a dashboard using Api",
+    link: " https://jimmabelle.github.io/assignment-vat-calculator/",
+    image: "images/newyork.jpeg"
+  }, {
+    id: "clock",
+    name: "Assignment Clock",
+    description: "Build a dashboard using Api",
+    link: " https://jimmabelle.github.io/assignment-vat-calculator/",
+    image: "images/office.jpeg"
   }
+
 ]
