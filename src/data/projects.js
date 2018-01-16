7a877796516aada75c5cd61658458a47cc624220
jimmabelle <jimmabelle@users.noreply.github.com>
2018-01-16 11:10:31 +0100

    images move out from the folder

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,67 +4,67 @@ export default [
     name: "Yearbook",
     description: "Build a yearbook using floats",
     link: "https://jimmabelle.github.io/assignment-yearbook",
-    image: "images/board.jpeg"
+    image: "/board.jpeg"
   }, {
     id: "hero",
     name: "Hero",
     description: "Create a responsive hero banner",
     link: "https://jimmabelle.github.io/assignment-hero/",
-    image: "images/book.jpeg"
+    image: "/book.jpeg"
   }, {
     id: "radio-player",
     name: "Radio Player",
     description: "A radio player using the Sveriges Radio API",
     link: "https://jimmabelle.github.io/assignment-radio-player/",
-    image: "images/building.jpeg"
+    image: "/building.jpeg"
   }, {
     id: "design-brief",
     name: "Design Brief",
     description: "Build a homepage as close as original",
     link: "https://jimmabelle.github.io/assignment-design-brief//",
-    image: "images/bulb.jpeg"
+    image: "/bulb.jpeg"
   }, {
     id: "flexbox",
     name: "Flexbox",
     description: "Build a yearbook using flexbox",
     link: "https://jimmabelle.github.io/assignment-flexbox/",
-    image: "images/calculation.jpeg"
+    image: "/calculation.jpeg"
   }, {
     id: "form",
     name: "Form",
     description: "Build a form",
     link: "https://jimmabelle.github.io/assignment-forms/",
-    image: "images/calendar.jpeg"
+    image: "/calendar.jpeg"
   }, {
     id: "faq",
     name: "FAQ",
     description: "Build a faq and make an accordion",
     link: "https://jimmabelle.github.io/assignment-accordion/",
-    image: "images/code.jpeg"
+    image: "/code.jpeg"
   }, {
     id: "weather-dashborad",
     name: "Weather Dashboard",
     description: "Build a dashboard using Api",
     link: "https://jimmabelle.github.io/assignment-weather/",
-    image: "images/keyboard.jpeg"
+    image: "/keyboard.jpeg"
   }, {
     id: "todo-app",
     name: "To Do App",
     description: "Build a todo list width local storage",
     link: "https://jimmabelle.github.io/assignment-todo-app-2/",
-    image: "images/metal.jpeg"
+    image: "/metal.jpeg"
   }, {
     id: "vat-calculator",
     name: "Vat Calculator",
     description: "Build a Calculator",
     link: " https://jimmabelle.github.io/assignment-vat-calculator/",
-    image: "images/newyork.jpeg"
+    image: "/newyork.jpeg"
   }, {
     id: "clock",
     name: "Assignment Clock",
     description: "Build a Clock",
     link: " https://jimmabelle.github.io/assignment-vat-calculator/",
-    image: "images/office.jpeg"
+    image: "/office.jpeg"
   }
 
 ]
