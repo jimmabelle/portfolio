6c7c2b564bd695ffe3845246e3c8e74a6b6e0c17
jimmabelle <jimmabelle@users.noreply.github.com>
2018-05-31 10:08:21 +0200

    updates

--- src/pages/projects/project/index.js
+++ src/pages/projects/project/index.js
@@ -1,35 +1,35 @@
-import React from "react"
-import projects from "data/projects"
-
-import "./index.css"
-
-export default class Project extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      project: {}
-    }
-  }
-
-  componentDidMount() {
-    const project = projects.find(p => (p.id === this.props.match.params.id))
-    this.setProject(project)
-  }
-
-  setProject = (project) => {
-    this.setState({
-      project
-    })
-  }
-
-  render() {
-    return (
-      <div className="ProjectPage">
-        <h1>{this.state.project.name}</h1>
-        <div className="projects">
-          {this.state.project.description}
-        </div>
-      </div>
-    )
-  }
-}
+// import React from "react"
+// import projects from "data/projects"
+//
+// import "./index.css"
+//
+// export default class Project extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       project: {}
+//     }
+//   }
+//
+//   componentDidMount() {
+//     const project = projects.find(p => (p.id === this.props.match.params.id))
+//     this.setProject(project)
+//   }
+//
+//   setProject = (project) => {
+//     this.setState({
+//       project
+//     })
+//   }
+//
+//   render() {
+//     return (
+//       <div className="ProjectPage">
+//         <h1>{this.state.project.name}</h1>
+//         <div className="projects">
+//           {this.state.project.description}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
